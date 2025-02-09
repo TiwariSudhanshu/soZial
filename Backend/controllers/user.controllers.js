ecf86423aa884811752ad72174acac8877c1d57f
Sudhanshu Tiwari <sudhanshutiwari9836@gmail.com>
2025-02-09 11:24:56 +0530

    Cookies error

--- Backend/controllers/user.controllers.js
+++ Backend/controllers/user.controllers.js
@@ -137,7 +137,8 @@ export const loginUser = asyncHandler(async (req, res) => {
     const options = {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // Ensure cookies are only sent over HTTPS
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Allow cross-origin cookies in production
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      domain: process.env.NODE_ENV === "production" && "sozial-server.onrender.com",
     };
   
 
